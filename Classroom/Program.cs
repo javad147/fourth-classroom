73df9ff0b2c495a1600d4c61505fa062d9b93e46
javad147 <javadnb@code.edu.az>
2023-09-15 14:57:55 +0400

    Classroom

---
+++ Classroom/Program.cs
@@ -0,0 +1,300 @@
+﻿// 2 Video Time 17:08
+//Console.WriteLine("Hello P418");
+//Console.ReadLine();
+
+//3 Video Time 12:15
+
+//int studentAge = 25;
+//string name1 = "XXX";
+//string name2 = "YYY";
+
+//string result = name2+ name1;
+//Console.WriteLine(result);
+
+
+// 3 Video Time 18:02
+
+
+//bool isOnline = true;
+
+//string name = "sdsds";
+
+//Console.WriteLine(isOnline.GetType());
+
+
+//bool isOnline = true;
+
+//if (isOnline)
+//{
+//    Console.WriteLine("Yes");
+//}
+//else 
+//{
+//    Console.WriteLine("No");
+//}
+
+
+// 4 Video Time 3:40
+
+//int age = 29;
+
+//if (age > 30)
+//{
+//    Console.WriteLine("Qocalirsan");
+//}
+//else if (age == 29)
+//{
+//    Console.WriteLine("Yaxshisan");
+
+//}
+//else
+//{
+//    Console.WriteLine("Gencsen");
+//}
+
+//4 Video Time Time 7:11
+
+//int number = 30;
+
+//string result = number > 30 ? "Qocasan" : "Yaxshisan";
+
+//Console.WriteLine(result);
+
+//bool res = number > 30 ? true : false;
+
+//if (number > 30)
+//{
+//    Console.WriteLine(true);
+//}
+//else 
+//{
+//    Console.WriteLine(false);
+//}
+
+// 4 Video Time 09:40
+
+//int x = 10, y = 100;
+
+//string result = x > y ? "x is greater than y":
+//                     x < y ? "x is less than y":
+//                         x == y ? "x is equal to y" : "no result";
+//Console.WriteLine(result);
+
+// 4 Video Time  12:10
+
+//string name = null;
+
+//var result = name ?? "XXX";
+
+//Console.WriteLine(result);
+
+
+//4 Video Time 17:52
+
+
+//Console.WriteLine("Add you email: ");
+
+//string email = Console.ReadLine();
+
+//Console.WriteLine("Add you password: ");
+
+//string password = Console.ReadLine();
+
+//if (email == "cavid@gmail.com" && password == "123")
+//{
+//    Console.WriteLine("Login success");
+//}
+//else 
+//{
+//    Console.WriteLine("Email or password is wrong");
+//}
+
+
+//4 Video Time 23:23
+
+
+//int weekDay = 2;
+
+//switch (weekDay) 
+//{
+//    case 1:
+//        Console.WriteLine("Monday");
+//        break;
+//    case 2:
+//        Console.WriteLine("Tuesday");
+//        break;
+//    case 3:
+//        Console.WriteLine("Wednesdya");
+//        break;
+//    default:
+//        Console.WriteLine("week day not found");
+//        break;
+
+//}
+
+
+//4 Video Time 30:00
+
+//int[] nums1 = new int[10];
+
+//int[] nums2 = new int[3] { 1, 4, 5 };
+
+//int[] nums = { 3, 2, 6, 8, 9 };
+
+//for (int i = 0; i < nums.Length; i++) 
+//    if (nums[i] == 2)
+//{
+//    Console.WriteLine(nums[i]);
+//        break;
+//}
+
+
+//5 Video Time 06:03
+
+//int[] nums = { 3, 2, 6, 8, 9 };
+
+//int count = 0;
+
+
+
+//for (int i = 0; i < nums.Length; i++)
+//{
+//    if (nums[i] == 2)
+//    {
+//        count++;
+//    }
+//}
+
+
+//int[] nums = { 3, 2, 6, 8, 9 };
+
+//int count = 0;
+
+
+
+
+//int[] nums = { 3, 2, 6, 8, 9 };
+
+//int sum = 0;
+
+
+//for (int i = 0; i < nums.Length; i++)
+//{
+//    if (nums[i] == 2)
+//    {
+//        sum += i;
+//    }
+//}
+//Console.WriteLine(sum);
+
+//int[] nums = { 3, 2, 6, 8, 9 };
+
+//int count = 0;
+
+//int sumElem = 0;
+
+//int sumIndex = 0;
+
+//boolisTrue = true; 
+
+
+//for (int i = 0; i < nums.Length; i++)
+//{
+//    if (nums[i] == 2)
+//    {
+//        sumElem += nums[i];
+
+//        sumIndex += i;
+//    }
+//}
+//if (IsTrue)
+//{
+//    IAsyncResult = sumElem - sumIndex;
+
+//}
+//else 
+//{
+//    Console.WriteLine("test");
+//}
+
+
+//5 Video Time 25:05
+
+//int[] nums = { 3, 2, 6, 2, 9, 2 };
+
+//for (int i = 0; i < nums.Length; i++)
+//{
+//    if (nums[i] == 2)
+//    {
+//        Console.WriteLine(nums[i]);
+//        break;
+//    }
+
+//}
+
+
+//for (int i = 0; i < nums.Length; i++)
+//{
+//    if (nums[i] == 2)
+//    {
+//        continue;
+//    }
+//    Console.WriteLine(nums[i]);
+//}
+
+
+//foreach (var item in nums) 
+//{
+//    if (item == 2) 
+//    {
+//        Console.WriteLine(item);
+//    }
+
+//}
+
+
+//List<int> indexes = new List<int>();
+
+//for (int i = 0; i < nums.Length; i++)
+//{
+//    if (nums[i] == 2)
+//    {
+//        indexes.Add(i);
+//    }
+//    Console.WriteLine(nums[i]);
+//}
+
+
+
+//6 Video Time 06:04
+
+//DateTime date = DateTime.Now;
+//Console.WriteLine(date);
+
+
+//DateTime date = DateTime.Now.AddDays(5);
+//Console.WriteLine(date);
+
+//DateTime date = DateTime.Now.AddDays(100);
+//Console.WriteLine(date);
+
+//string date = DateTime.Now.ToString("MM/dd/yyyy HH:mm");
+//Console.WriteLine(date);
+
+
+//int num = 100;
+//string res = num.ToString();
+
+////6 Video Time  18:15
+
+//int[] nums = { 1, 4, 5, 8, 6, 3 };
+//int result = 1;
+
+//for (int i = 0; i < nums.Length; i++) 
+//{
+//    if (nums[i] % 2 == 0) 
+//    {
+//        result += nums[i];
+//    }
+//}
+//Console.WriteLine(result);
